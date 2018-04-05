919b3e9f01a2df23028d3940a49e313593b984be
zzc1393925530 <1393925530@qq.com>
2018-04-05 09:04:25 +0800

    imagePreview
    增加图片预览功能..

---
+++ index.js
@@ -0,0 +1,16 @@
+$(function() {
+$("div.container img").mouseover(function (e) {
+    this.myTitle = this.title;
+    this.title = "";
+    var tooltip = "<div id = 'tooltip'><img src = '" + this.src + "' alt = '预览' />" + "</div>";
+    $("body").append(tooltip);
+    $("#tooltip")
+    .css({
+        "top": (e.pageY) + "px",
+        "left": (e.pageX) + "px"
+    }).show("fast");
+}).mouseout(function() {
+    this.title = this.myTitle;
+    $("#tooltip").remove();
+});
+})
