6e46dc4e258e210910926c3ef7caa1d744233efe
zzc1393925530 <1393925530@qq.com>
2018-04-25 15:46:17 +0800

    addComment
    加入注释以便理解BOOTSTRAP NAVBAR

--- index.js
+++ index.js
@@ -15,22 +15,22 @@ $("div.container img").mouseover(function (e) {
 });
 })*/  
 //jquery
-$('img.lazy').lazyload({
+$('img.lazy').lazyload({  //图片懒加载
     effect: "fadeIn"
 });
 $(document).ready(function () {
 
 
     // Calculating the distance from the top of the page to the initial position of the navbar, and store it in a variable
-    var navbarOffest = $('.navbar').offset().top;
+    var navbarOffest = $('.navbar').offset().top;  //获取导航栏相对偏移量
 
 
-    $(window).on('scroll', function () {
+    $(window).on('scroll', function () {  //滚动事件
 
-        var navbarHeight = $('.navbar').outerHeight();
+        var navbarHeight = $('.navbar').outerHeight();  //获取导航栏的外部高度
 
-        if ($(window).scrollTop() >= navbarOffest) {
-            $('.navbar').addClass('navbar-fixed-top');
+        if ($(window).scrollTop() >= navbarOffest) { //网页的滚动高度与相对偏移量对比
+            $('.navbar').addClass('navbar-fixed-top'); //如果滚动高度大于相对偏移量则将body的apdding-top属性设置为外部高度
             $('body').css('padding-top', navbarHeight + 'px');
         } else {
             $('.navbar').removeClass('navbar-fixed-top');
